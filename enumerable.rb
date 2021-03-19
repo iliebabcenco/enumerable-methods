fbe159baf40c2b00e1b6951f04c5bb90bffcca3e
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-03-19 12:07:57 +0200

    add my_each_index method

--- enumerable.rb
+++ enumerable.rb
@@ -1,13 +1,25 @@
 module Enumerable
     def my_each
-      
       for i in self
         yield i
       end
       self
     end
-     a = [1, 3, 4, 8]
-     a.my_each { |element| puts element + 7 }
+    a = [1, 3, 4, 8]
+    a.my_each { |element| puts element + 7 }
+
+    def my_each_index
+      counter = 0
+      for i in self
+        yield i, counter
+        counter += 1
+      end
+      self
+    end
+
+    a1 = ["ilie", "henry", "dasdas0", "dasdsa"]
+    a1.my_each_index { |element, index| p "This is our #{element} and its index = #{index}" }
+
      
   end
 
