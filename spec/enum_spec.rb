a7a816ad26bc1950e8e29b9e440781add9c67a58
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-02 11:44:42 +0300

    add tests for #my_each

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,1 +1,17 @@
 require './enumerable'
+
+describe 'Enumerable' do
+    let(:ex) {[2, 4, 5, 8, 1]}
+    let(:ex_hash) {{ 0 => 2, 1 => 4, 2 => 5, 3 => 8, 4 => 1 }}
+    describe '#my_each' do
+      it 'return an Enumerator if no block' do
+        expect(ex.my_each).to be_a(Enumerator)
+      end
+      it 'repeat the code from block with each element' do
+        arr = []
+        ex.my_each { |x| arr << x }
+        expect(arr).to eql(ex)
+      end
+    end
+
+end
